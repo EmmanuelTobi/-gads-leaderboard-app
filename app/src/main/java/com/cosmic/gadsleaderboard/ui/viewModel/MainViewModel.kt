59feca6eee739226aa012e90bab74540430e1bb8
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-09-12 13:28:17 +0100

    viewModel updated for submission

--- app/src/main/java/com/cosmic/gadsleaderboard/ui/viewModel/MainViewModel.kt
+++ app/src/main/java/com/cosmic/gadsleaderboard/ui/viewModel/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.cosmic.gadsleaderboard.ui.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.cosmic.gadsleaderboard.apiservices.ApiHelper
+import com.cosmic.gadsleaderboard.apiservices.submissionHelper
 import com.cosmic.gadsleaderboard.utils.Resource
 import kotlinx.coroutines.Dispatchers
 
@@ -37,4 +38,29 @@ class MainViewModel(private val apiHelper: ApiHelper) : ViewModel() {
 
         }
     }
+
+}
+
+class SubmissionViewModel(private val submissionHelper: submissionHelper) : ViewModel() {
+
+    fun makeSubmission(firstName: String, lastName: String, emailAddress: String, projectLink: String) = liveData(Dispatchers.IO) {
+
+        submissionHelper.emailAddress = emailAddress
+        submissionHelper.firstName = firstName
+        submissionHelper.lastName = lastName
+        submissionHelper.projectLink = projectLink
+
+        emit(Resource.loading(data = null))
+
+        try {
+
+            emit(Resource.success(data = submissionHelper.makeSubmission()))
+
+        } catch (exception: Exception) {
+
+            emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
+
+        }
+    }
+
 }
