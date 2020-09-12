dce4ae15e4978b4145da2b07c38c3ccc7d23ba1d
EmmanuelTobi <emmanuel.a.oluwatobi@gmail.com>
2020-09-12 13:27:34 +0100

    api updated for submission

--- app/src/main/java/com/cosmic/gadsleaderboard/apiservices/ApiClient.kt
+++ app/src/main/java/com/cosmic/gadsleaderboard/apiservices/ApiClient.kt
@@ -8,9 +8,12 @@ import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
+import retrofit2.http.*
+
+private const val SUBMIT_URL = "https://docs.google.com/forms/d/e/1FAIpQLSf9d1TcNU6zc6KR8bSEM41Z1g1zl35cwZr2xyjIhaMAz8WChQ/formResponse"
 
 object ApiClient {
 
@@ -35,5 +38,14 @@ interface APIInterface {
 
     @GET("/api/skilliq")
     suspend fun leaderboardBySkillIQ(): List<LeaderboardScoresModelItem>
+
+    @POST
+    suspend fun submitProject(
+        @Url url: String = SUBMIT_URL,
+        @Field("entry.1877115667") firstName: String,
+        @Field("entry.2006916086") lastName: String,
+        @Field("entry.1824927963") emailAddress: String,
+        @Field("entry.284483984") projectLink: String
+    ): Response<Void>
 
 }
--- app/src/main/java/com/cosmic/gadsleaderboard/apiservices/ApiHelper.kt
+++ app/src/main/java/com/cosmic/gadsleaderboard/apiservices/ApiHelper.kt
@@ -4,5 +4,10 @@ class ApiHelper(private val apiInteface: APIInterface) {
 
     suspend fun getLeaderBoardByHours() = apiInteface.leaderboardByHours()
     suspend fun getLeaderBoardBySkillIQ() = apiInteface.leaderboardBySkillIQ()
+}
+
+class submissionHelper(private val apiInteface: APIInterface, var firstName: String, var lastName: String, var emailAddress: String, var projectLink: String) {
+
+    suspend fun makeSubmission() = apiInteface.submitProject(firstName = firstName, lastName = lastName, emailAddress = emailAddress, projectLink = projectLink);
 
 }
